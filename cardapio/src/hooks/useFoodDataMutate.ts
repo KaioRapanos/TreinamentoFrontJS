6dcf9fbffdc876c58eb1e0f501d1f0903c6b9baf
KaioRapanos <kaiorapanos@gmail.com>
2024-06-03 19:31:00 -0300

    Update

--- cardapio/src/hooks/useFoodDataMutate.ts
+++ cardapio/src/hooks/useFoodDataMutate.ts
@@ -40,27 +40,3 @@ export function useFoodDataMutate(): {
         isLoading: mutation.isLoading
     };
 }
-
-/*import axios from "axios";
-import { FoodData } from "../interface/FoodData";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-
-const API_URL = 'http://localhost:8080';
-
-const postData = async (data: FoodData): Promise<FoodData> => {
-    const response = await axios.post(API_URL + '/food', data);
-    return response.data;  // Assuming your API returns the saved data in the response
-}
-
-export function useFoodDataMutate() {
-    const queryClient = useQueryClient();
-    const mutate = useMutation({
-        mutationFn: postData,
-        retry: 2,
-        onSuccess: () => {
-            queryClient.invalidateQueries(['food-data'])
-        }
-    });
-
-    return mutate;
-}*/
